5a326f3ddba594cc4bd6268bbffb7740e0e40626
hsyi <531856459@qq.com>
2017-06-17 13:51:56 +0800

    x

--- testsuit.sh
+++ testsuit.sh
@@ -14,9 +14,9 @@ echo $hadoop_test_dir
 rm TestDFSIO_results.log
 echo "开始极小文件测试！"
 echo "-----------------------------------------------------------------------"
-echo "读写10000个10B的文件"
-hadoop jar $jar_path $main_class -write -nrFiles 1000 -size "10B"
-hadoop jar $jar_path $main_class -read -nrFiles 1000 -size "10B"
+echo "读写100个10B的文件"
+hadoop jar $jar_path $main_class -write -nrFiles 100 -size "10B"
+hadoop jar $jar_path $main_class -read -nrFiles 100 -size "10B"
 hadoop jar $jar_path $main_class -clean
 echo "开始巨文件测试!"
 echo "-----------------------------------------------------------------------"
@@ -27,13 +27,12 @@ linenum=0
 avtimesec=0
 num_testdfsio=0
 while read line || [ -n "$line" ];do
-    if ($(linenum%9)==0)
+    if [ $(($linenum % 9 ))];then
         $tmp=$(echo $line | awk -F, '{print $2}')
         let avtimesec=avtimesec+tmp
         let num_testdfsio=num_testdfsio+1
-    then
     fi
-    (++linenum);
+    let linenum=linenum+1
 done < TestDFSIO_results.log
 let avtimesec=avtimesec/num_testdfsio
 echo "平均执行时间：$avtimesec"
